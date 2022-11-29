805352cd906cfffca00905301eac15bba4165ccb
Kiran2321 <kirankc151@gmail.com>
2022-11-29 13:06:14 +0530

    kuki

---
+++ if_statement.java
@@ -0,0 +1,13 @@
+public class if_statement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str1="kiran";
+		String str2="karthik";
+if(str1=="kiran")
+{
+	System.out.println("welcome to Eclipse");
+}
+	}
+
+}
